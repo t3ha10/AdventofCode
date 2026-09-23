08575f8f53f867d8c43f5d2bc0287da378e3c28b
Tran Thi Thu Ha <120727195+t3ha10@users.noreply.github.com>
2026-09-23 13:19:49 +0300

    update day_2015_2

---
+++ 2015/2/aoc2015_2.py
@@ -0,0 +1,27 @@
+file = open(r'input_2015_2.txt', 'rt')
+DIMENSIONS = file.read()
+total_paper = 0
+
+# for line in DIMENSIONS.strip().splitlines():
+#     present = line.split('x')
+
+#     l = int(present[0])
+#     w = int(present[1])
+#     h = int(present[2])
+
+#     present_area = 2*l*w + 2*w*h + 2*h*l
+#     present_extra = min(l*w, w*h, h*l)
+
+#     present_paper = 2*l*w + 2*w*h + 2*h*l + present_extra
+#     total_paper += present_paper
+
+# print(f'{total_paper = }')
+
+# Toinen tapa (lyhyempi mutta sekävämpi)
+total_paper = 0
+
+for line in DIMENSIONS.strip().splitlines():
+    l, w, h = map(int, line.split('x'))
+    total_paper += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
+
+print(f'{total_paper = }')
