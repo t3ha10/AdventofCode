cc805c347b115c39a744260dc71a42831cfb0137
Tran Thi Thu Ha <120727195+t3ha10@users.noreply.github.com>
2026-09-23 14:12:23 +0300

    fix day_2015_2

--- 2015/2/aoc2015_2.py
+++ 2015/2/aoc2015_2.py
@@ -1,6 +1,32 @@
 file = open(r'input_2015_2.txt', 'rt')
 DIMENSIONS = file.read()
-total_paper = 0
+
+sum_paper = 0
+sum_ribbon = 0
+for line in DIMENSIONS.split():
+    present = line.split('x')
+    l = int(present[0])
+    w = int(present[1])
+    h = int(present[2])
+    present_area = 2*l*w + 2*w*h + 2*h*l
+    sum_paper += present_area
+    present_extra = min(l*w, w*h, h*l)
+    sum_paper += present_extra
+    smallest_perimeter = 2 * (min(l+w, w+h, h+l))
+    sum_ribbon += smallest_perimeter
+    bow = l * w * h
+    sum_ribbon += bow
+ 
+print(f"total paper is {sum_paper}")
+print(f"total ribbon is {sum_ribbon}")
+# total_paper = 0
+
+# Toinen tapa (lyhyempi mutta sekävämpi)
+# for line in DIMENSIONS.strip().splitlines():
+#     l, w, h = map(int, line.split('x'))
+#     total_paper += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
+
+# print(f'{total_paper = }')
 
 # for line in DIMENSIONS.strip().splitlines():
 #     present = line.split('x')
@@ -16,12 +42,3 @@ total_paper = 0
 #     total_paper += present_paper
 
 # print(f'{total_paper = }')
-
-# Toinen tapa (lyhyempi mutta sekävämpi)
-total_paper = 0
-
-for line in DIMENSIONS.strip().splitlines():
-    l, w, h = map(int, line.split('x'))
-    total_paper += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
-
-print(f'{total_paper = }')
